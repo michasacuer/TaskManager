834055ed15e3643adfa2763126d70d2dc19d5283
michasacuer <michasacuer3@gmail.com>
2019-07-05 19:11:44 +0200

    Added: Notification entity

---
+++ Src/TaskManager.Domain/Entity/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Domain.Entity
+{
+    using TaskManager.Domain.Entity.Base;
+
+    public class Notification : BaseEntity<int>
+    {
+        public string Message { get; set; }
+    }
+}
