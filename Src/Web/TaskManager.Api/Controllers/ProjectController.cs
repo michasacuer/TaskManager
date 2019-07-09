c72bbf77caf30fb419a0b0e8569e2968fb315bb9
michasacuer <michasacuer3@gmail.com>
2019-07-09 13:42:17 +0200

    Added: ProjectController and Web functional tests project

---
+++ Src/Web/TaskManager.Api/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManager.Api.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using TaskManager.Application.Commands;
+
+    public class ProjectController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateProject([FromBody]CreateProjectCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
