65f52a5d32850438eaa6190dc3dcbff1a54f528b
michasacuer <michasacuer3@gmail.com>
2019-07-19 10:25:21 +0200

    Added: Repository pattern

---
+++ Src/Web/TaskManager.Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManager.Application.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepository<T>
+        where T : class
+    {
+        Task<T> GetById(int id);
+
+        Task<List<T>> GetAll();
+
+        Task Add(T item);
+
+        Task Update(T item);
+
+        Task Delete(T item);
+    }
+}
---
+++ Src/Web/TaskManager.Persistence/Repository.cs
@@ -0,0 +1,46 @@
+﻿namespace TaskManager.Persistence
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using TaskManager.Application.Interfaces;
+
+    public class Repository<T> : IRepository<T> 
+        where T : class
+    {
+        protected readonly TaskManagerDbContext _context;
+
+        public Repository(TaskManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(T item)
+        {
+            await _context.Set<T>().AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T item)
+        {
+            _context.Set<T>().Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
