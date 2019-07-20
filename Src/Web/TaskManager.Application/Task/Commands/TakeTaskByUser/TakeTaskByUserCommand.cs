99f665ba949db2c861f92255a9837778910726d9
michasacuer <michasacuer3@gmail.com>
2019-07-20 09:14:11 +0200

    Added tasks

---
+++ Src/Web/TaskManager.Application/Task/Commands/TakeTaskByUser/TakeTaskByUserCommand.cs
@@ -0,0 +1,56 @@
+﻿namespace TaskManager.Application.Task.Commands.TakeTaskByUser
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentValidation;
+    using MediatR;
+    using TaskManager.Application.Interfaces;
+    using TaskManager.Common.Exceptions;
+
+    public class TakeTaskByUserCommand : IRequest
+    {
+        public int TaskId { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        public class Handler : IRequestHandler<TakeTaskByUserCommand>
+        {
+            private readonly IApplicationUserRepository applicationUserRepository;
+
+            private readonly ITaskRepository taskRepository;
+
+            public Handler(IApplicationUserRepository applicationUserRepository, ITaskRepository taskRepository)
+            {
+                this.applicationUserRepository = applicationUserRepository;
+                this.taskRepository = taskRepository;
+            }
+
+            public async Task<Unit> Handle(TakeTaskByUserCommand request, CancellationToken cancellationToken)
+            {
+                await new TakeTaskByUserCommandValidator().ValidateAndThrowAsync(request);
+
+                var user = await this.applicationUserRepository.GetByIdAsync(request.ApplicationUserId)
+                    ?? throw new EntityNotFoundException();
+
+                if (await this.applicationUserRepository.UserInRoleAsync(user, "Manager")
+                    || await this.applicationUserRepository.UserInRoleAsync(user, "Developer"))
+                {
+                    var task = await this.taskRepository.GetByIdAsync(request.TaskId)
+                        ?? throw new EntityNotFoundException();
+
+                    await new TaskApplicationUserIdValidator().ValidateAndThrowAsync(task);
+
+                    task.ApplicationUserId = user.Id;
+                    this.taskRepository.Update(task);
+                    await this.taskRepository.SaveAsync(cancellationToken);
+                }
+                else
+                {
+                    throw new UserHaveNoPermissionException();
+                }
+
+                return Unit.Value;
+            }
+        }
+    }
+}
---
+++ Src/Web/TaskManager.Application/Task/Commands/TakeTaskByUser/TakeTaskByUserCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskManager.Application.Task.Commands.TakeTaskByUser
+{
+    using FluentValidation;
+    using TaskManager.Domain.Entity;
+
+    public class TakeTaskByUserCommandValidator : AbstractValidator<TakeTaskByUserCommand>
+    {
+        public TakeTaskByUserCommandValidator()
+        {
+            RuleFor(x => x.ApplicationUserId).NotEmpty();
+        }
+    }
+
+    public class TaskApplicationUserIdValidator : AbstractValidator<ToDoTask>
+    {
+        public TaskApplicationUserIdValidator()
+        {
+            RuleFor(x => x.ApplicationUserId).Empty();
+        }
+    }
+}
