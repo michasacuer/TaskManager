8c9c918f95917e7ab68bfc1373b701c039e72887
michasacuer <michasacuer3@gmail.com>
2019-12-17 13:57:05 +0100

    WebUI: Initial commit

---
+++ Src/web-ui/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>It will be Task Manager here soon</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
