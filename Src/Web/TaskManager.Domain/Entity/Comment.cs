261da572c8bacc402c2a73e2aa21f360502ece36
michasacuer <michasacuer3@gmail.com>
2020-03-08 09:27:26 +0100

    API: Added Comment entity

---
+++ Src/Web/TaskManager.Domain/Entity/Comment.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskManager.Domain.Entity
+{
+    using System;
+    using TaskManager.Domain.Entity.Base;
+    
+    public class Comment : BaseEntity<int>
+    {
+        public int ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int ToDoTaskId { get; set; }
+
+        public ToDoTask ToDoTask { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
