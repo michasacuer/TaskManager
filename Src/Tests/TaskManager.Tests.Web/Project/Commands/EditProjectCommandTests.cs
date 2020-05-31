b44bead16e6a99b3eb676ed7df06c68038388c82
michasacuer <michasacuer3@gmail.com>
2020-05-31 08:25:19 +0200

    Tests: Services to Moq in EditProjectCommandTests.cs

--- Src/Tests/TaskManager.Tests.Web/Project/Commands/EditProjectCommandTests.cs
+++ Src/Tests/TaskManager.Tests.Web/Project/Commands/EditProjectCommandTests.cs
@@ -3,118 +3,81 @@
     using System.Threading;
     using System.Threading.Tasks;
     using FluentValidation;
+    using Moq;
     using Shouldly;
     using Xunit;
     using TaskManager.Persistence;
     using TaskManager.Persistence.Repository;
     using TaskManager.Tests.Infrastructure;
     using TaskManager.Application.Commands.EditProject;
+    using TaskManager.Application.Interfaces;
     using TaskManager.Domain.Entity;
     using TaskManager.Common.Exceptions;
 
-    [Collection("ServicesTestCollection")]
     public class EditProjectCommandTests
     {
-        private readonly TaskManagerDbContext context;
+        private readonly Mock<IProjectRepository> projectRepositoryMock;
 
-        private readonly ProjectRepository projectRepository;
+        private readonly EditProjectCommand.Handler uut;
 
-        public EditProjectCommandTests(ServicesFixture fixture)
+        public EditProjectCommandTests()
         {
-            this.context = fixture.Context;
-            this.projectRepository = new ProjectRepository(fixture.Context);
+            this.projectRepositoryMock = new Mock<IProjectRepository>();
+            this.uut = new EditProjectCommand.Handler(projectRepositoryMock.Object);
         }
 
-        [Fact]
-        public async Task EditProjectShouldEditProjectInDatabase()
+        [Theory]
+        [NoRecurseAutoData]
+        public void EditProjectShouldEditProjectInDatabase(
+            EditProjectCommand command,
+            Project updatedProject,
+            Project projectToUpdate)
         {
-            var projectBefore = await this.context.Projects.FindAsync(1);
-            string nameBefore = projectBefore.Name;
+            updatedProject.Id = projectToUpdate.Id;
+            command.Data.Id = projectToUpdate.Id;
+            
+            this.projectRepositoryMock
+                .Setup(x => 
+                    x.GetByIdAsync(It.Is<int>(z => z == command.Data.Id)))
+                .ReturnsAsync(projectToUpdate);
+            
+            this.projectRepositoryMock.Setup(x => x.Update(It.Is<Project>(z => z == updatedProject)));
+            this.projectRepositoryMock.Setup(x => x.SaveAsync(CancellationToken.None)).Returns(Task.CompletedTask);
 
-            var command = new EditProjectCommand
-            {
-                Data = new Project
-                {
-                    Id = 1,
-                    Name = "ChangedName!",
-                    Description = "ChangedDesc!"
-                }
-            };
+            var result = this.uut.Handle(command, CancellationToken.None).Result;
 
-            var commandHandler = new EditProjectCommand.Handler(this.projectRepository);
-
-            await commandHandler.Handle(command, CancellationToken.None);
-
-            var project = await this.context.Projects.FindAsync(1);
-
-            project.ShouldNotBeNull();
-            project.Name.ShouldBe(command.Data.Name);
-            nameBefore.ShouldNotBe(project.Name);
+            result.ShouldNotBeNull();
         }
 
-        [Fact]
-        public async Task EditProjectShouldThrowWhenProjectNotFound()
+        [Theory]
+        [NoRecurseAutoData]
+        public void EditProjectShouldThrowWhenProjectNotFound(
+            EditProjectCommand command,
+            Project projectToUpdate)
         {
-            var command = new EditProjectCommand
-            {
-                Data = new Project
-                {
-                    Id = 1111111111,
-                    Name = "ChangedName!",
-                    Description = "ChangedDesc!"
-                }
-            };
-
-            var commandHandler = new EditProjectCommand.Handler(this.projectRepository);
-
-            await commandHandler.Handle(command, CancellationToken.None).ShouldThrowAsync<EntityNotFoundException>();
+            projectToUpdate = null;
+            
+            this.projectRepositoryMock
+                .Setup(x => 
+                    x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(projectToUpdate);
+            
+            this.uut.Handle(command, CancellationToken.None).ShouldThrowAsync<EntityNotFoundException>();
         }
 
-        [Fact]
-        public async Task EditProjectShouldThrowWhenProjectNameIsEmpty()
+        [Theory]
+        [NoRecurseAutoData]
+        public void EditProjectShouldThrowWhenProjectNameIsEmpty(
+            EditProjectCommand command,
+            Project projectToUpdate)
         {
-            var command = new EditProjectCommand
-            {
-                Data = new Project
-                {
-                    Id = 1,
-                    Name = "",
-                    Description = "ChangedDesc!"
-                }
-            };
-
-            var commandHandler = new EditProjectCommand.Handler(this.projectRepository);
-
-            await commandHandler.Handle(command, CancellationToken.None).ShouldThrowAsync<ValidationException>();
-        }
-
-        [Fact]
-        public async Task EditProjectShouldEditWhenDescOnlyEdited()
-        {
-            var projectBefore = await this.context.Projects.FindAsync(7);
-            string nameBefore = projectBefore.Name;
-            string descriptionBefore = projectBefore.Description;
-
-            var command = new EditProjectCommand
-            {
-                Data = new Project
-                {
-                    Id = 7,
-                    Name = nameBefore,
-                    Description = "ChangedDescAAAAAAA!"
-                }
-            };
-
-            var commandHandler = new EditProjectCommand.Handler(this.projectRepository);
-
-            await commandHandler.Handle(command, CancellationToken.None);
-
-            var project = await this.context.Projects.FindAsync(7);
-
-            project.ShouldNotBeNull();
-            project.Name.ShouldBe(command.Data.Name);
-            nameBefore.ShouldBe(project.Name);
-            descriptionBefore.ShouldNotBe(project.Description);
+            command.Data.Name = null;
+            this.projectRepositoryMock
+                .Setup(x => 
+                    x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(projectToUpdate);
+            
+            this.uut.Handle(command, CancellationToken.None).ShouldThrowAsync<EntityNotFoundException>();
         }
     }
 }
